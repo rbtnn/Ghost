e510d2d48bcf39de55c333e9bab084b3789fe002
rbtnn <naru123456789@gmail.com>
2021-10-20 22:51:18 +0900

    fix

--- core/server/api/canary/bsm-podcast.js
+++ core/server/api/canary/bsm-podcast.js
@@ -1,5 +1,4 @@
-const i18n = require('../../../shared/i18n');
-const errors = require('@tryghost/errors');
+const config = require('../../../shared/config');
 const request = require('@tryghost/request');
 const models = require('../../models');
 
@@ -20,7 +19,7 @@ module.exports = {
                 const memberRows = await models.Member.getFilteredCollectionQuery({}).select('members.*').distinct();
                 for (var i = 0; i < memberRows.length; i++) {
                     if ((memberRows[i].uuid === frame.data.uuid) && (memberRows[i].status !== 'free')) {
-                        const response = await request('https://test.backspace.fm/feeds/f3f5805fe279e95b9d46f4623f7d998b409371f2658dab0fcec41d70b5bcb5ae/', {
+                        const response = await request(config.get('bsm_podcast').url, {
                             headers: {
                                 accept: 'text/xml'
                             },
