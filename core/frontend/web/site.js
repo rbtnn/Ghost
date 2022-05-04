452b375b95911148402eadead557f1ad2cc67a87
rbtnn <naru123456789@gmail.com>
2022-05-05 04:25:12 +0900

    fix

--- core/frontend/web/site.js
+++ core/frontend/web/site.js
@@ -68,6 +68,7 @@ module.exports = function setupSiteApp(options = {}) {
     // Serve images for backspacefm theme
 	for (var n = 1; n < 1000; n++) {
 		siteApp.use(mw.servePublicFile('built', 'public/backspacefm/' + ('0000' + n).slice(-3) + '.png', 'image/png', constants.ONE_YEAR_S));
+		siteApp.use(mw.servePublicFile('built', 'public/backspacefm/' + ('0000' + n).slice(-3) + '.jpg', 'image/jpg', constants.ONE_YEAR_S));
 	}
 
     // Card assets
