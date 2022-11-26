8df94c9da610fab8736bc2e376fa051ee49efdfe
rbtnn <naru123456789@gmail.com>
2022-11-26 15:58:29 +0900

    fix

--- ghost/core/core/server/api/endpoints/utils/serializers/output/utils/post-gating.js
+++ ghost/core/core/server/api/endpoints/utils/serializers/output/utils/post-gating.js
@@ -11,23 +11,22 @@ const forPost = (attrs, frame) => {
     const memberHasAccess = membersService.contentGating.checkPostAccess(attrs, frame.original.context.member);
 
     if (!memberHasAccess) {
-        attrs.excerpt = htmlToPlaintext.excerpt(attrs.html).substring(0, 300);
-        //const paywallIndex = (attrs.html || '').indexOf('<!--members-only-->');
-        //
-        //if (paywallIndex !== -1) {
-        //    attrs.html = attrs.html.slice(0, paywallIndex);
-        //    attrs.plaintext = htmlToPlaintext.excerpt(attrs.html);
-        //
-        //    if (!attrs.custom_excerpt && attrs.excerpt) {
-        //        attrs.excerpt = attrs.plaintext.substring(0, 500);
-        //    }
-        //} else {
-        //    ['plaintext', 'html', 'excerpt'].forEach((field) => {
-        //        if (attrs[field] !== undefined) {
-        //            attrs[field] = '';
-        //        }
-        //    });
-        //}
+        const paywallIndex = (attrs.html || '').indexOf('<!--members-only-->');
+
+        if (paywallIndex !== -1) {
+            attrs.html = attrs.html.slice(0, paywallIndex);
+            attrs.plaintext = htmlToPlaintext.excerpt(attrs.html);
+
+            if (!attrs.custom_excerpt && attrs.excerpt) {
+                attrs.excerpt = attrs.plaintext.substring(0, 500);
+            }
+        } else {
+            ['plaintext', 'html', 'excerpt'].forEach((field) => {
+                if (attrs[field] !== undefined) {
+                    attrs[field] = '';
+                }
+            });
+        }
     }
 
     if (!Object.prototype.hasOwnProperty.call(frame.options, 'columns') || (frame.options.columns.includes('access'))) {
